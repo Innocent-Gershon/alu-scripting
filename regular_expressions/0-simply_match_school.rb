1ed9079d553f3d57ce0042720bdc88658fffdf37
Innocent-Gershon <i.nangah@alustudent.com>
2024-09-03 03:16:37 -0700

    Simply school Matching

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/School/).join
+
+# Check if an argument is provided
+if ARGV.length != 1
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Get the argument
+input_string = ARGV[0]
+
+# Define the regular expression
+regex = /School/
+
+# Check if the input string matches the regular expression
+if input_string.match?(regex)
+  puts "Match found!"
+else
+  puts "No match found."
+end
+
