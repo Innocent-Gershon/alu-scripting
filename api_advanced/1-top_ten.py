f7de0bce14e606881bdffded491416cb76aec91a
Innocent-Gershon <i.nangah@alustudent.com>
2024-10-24 18:29:14 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""Script that fetches 10 hot posts for a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
-
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, print None."""
-    
+    """Return the titles of the first 10 hot posts for the given subreddit.
+    If the subreddit is not valid, print None.
+    """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = f"https://reddit.com/r/{subreddit}/hot.json?limit=10"
+    subreddit_url = "https://reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        for i in range(10):
-            print(json_data.get('data').get('children')[i].get('data').get('title'))
+        posts = json_data.get('data', {}).get('children', [])
+        if posts:
+            for post in posts:
+                print(post.get('data', {}).get('title'))
+        else:
+            print(None)
     else:
-        print(None)  # Handle both non-existent and other errors
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        top_ten(sys.argv[1])
-    else:
-        print("Usage: ./script.py <subreddit>")
+        print(None)
