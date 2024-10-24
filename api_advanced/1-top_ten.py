14f8367304c1fa8da1f3b76c408187a3ed8537d4
Innocent-Gershon <i.nangah@alustudent.com>
2024-10-24 18:15:11 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -11,6 +11,10 @@ def top_ten(subreddit):
     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
+    def output_check(subreddit):
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+
     if response.status_code == 200:
         json_data = response.json()
         for i in range(10):
@@ -23,27 +27,9 @@ def top_ten(subreddit):
     else:
         print(None)
 
-def output_check(subreddit):
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an error for HTTP issues
-        data = response.json()
 
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children']:
-                print(post['data']['title'])
-        else:
-            print(None)
+  
 
-    except requests.exceptions.HTTPError as err:
-        if err.response.status_code == 404:
-            print(None)  # Subreddit not found
-        else:
-            print(None)
 
-    except ValueError:
-        print(None)  # Handle JSON decode errors
 
